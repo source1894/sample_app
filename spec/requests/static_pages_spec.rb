eb388a620af655e20e9b997d1fcf37cd9514d4b9
Igor <source1894@gmail.com>
2015-08-16 09:37:21 +0300

    Just forgoten to add commit after exercise...I added function let in /static_pages_spec.rb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
     it "It should have the content 'Sample App'" do
@@ -9,7 +11,7 @@ describe "StaticPages" do
     end
     it "should have the right title" do
 	  visit '/static_pages/home'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+	  expect(page).to have_title("#{base_title} | Home")
   	end
   end
 
@@ -20,7 +22,7 @@ describe "Help page" do
     end
     it "should have the right title" do
 	  visit '/static_pages/help'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+	  expect(page).to have_title("#{base_title} | Help")
   	end
   end
 
@@ -32,7 +34,7 @@ describe "About page" do
   end
     it "should have the right title" do
 	  visit '/static_pages/about'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+	  expect(page).to have_title("#{base_title} | About Us")
   	end
 describe "Contact" do
     it "It should have the content 'Contact'" do
@@ -42,7 +44,7 @@ describe "Contact" do
   end
     it "should have the right title" do
 	  visit '/static_pages/contact'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+	  expect(page).to have_title("#{base_title} | Contact")
   	end
 
 end
