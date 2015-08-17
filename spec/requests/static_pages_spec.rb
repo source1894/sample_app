4fcc509fa49c12953f0971a9c672b0ff0e1885a8
Igor <source1894@gmail.com>
2015-08-17 17:14:31 +0300

    Add new test for Home_page that not have title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  
+  base_title = "Ruby on Rails Tutorial Sample App"
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
@@ -11,8 +11,12 @@ describe "StaticPages" do
     end
     it "should have the right title" do
 	  visit '/static_pages/home'
-	  expect(page).to have_title("#{base_title} | Home")
+	  expect(page).to have_title("#{base_title}")
   	end
+    it 'should not have a custom page title' do 
+      visit '/static_pages/home'
+    expect(page).not_to have_title('| Home')
+    end
   end
 
 describe "Help page" do
